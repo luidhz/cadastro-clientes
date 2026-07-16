52cc9588df185070fd0114a162e24e8d06345183
Brenno <ubrennz@gmail.comm>
2026-07-16 13:58:57 -0300

    feat: repository, service e resource da entidade produto

---
+++ src/main/java/com/luiz/cadastroclientes/repository/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.luiz.cadastroclientes.repository;
+
+import com.luiz.cadastroclientes.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    Optional<Produto> findByCodigoDeBarras(String codigoDeBarras);
+
+    Long id(Long id);
+}
---
+++ src/main/java/com/luiz/cadastroclientes/resource/ProdutoResource.java
@@ -0,0 +1,49 @@
+package com.luiz.cadastroclientes.resource;
+
+import com.luiz.cadastroclientes.entity.Produto;
+import com.luiz.cadastroclientes.service.ProdutoService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+@AllArgsConstructor
+public class ProdutoResource {
+    private final ProdutoService produtoService;
+
+    @PostMapping
+    public ResponseEntity<Produto> insert(@RequestBody Produto produto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(produtoService.insert(produto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> findAll() {
+        return ResponseEntity.ok().body(produtoService.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Produto> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(produtoService.findById(id));
+    }
+
+    @GetMapping(value = "/codigo-barras")
+    public ResponseEntity<Produto> findByCodigoDeBarras(@RequestParam String codigoDeBarras) {
+        return ResponseEntity.ok().body(produtoService.findByCodigoDeBarras(codigoDeBarras));
+    }
+
+    @PutMapping("/{id}")
+    public Produto update(@PathVariable Long id, @RequestBody Produto produto) {
+        return produtoService.update(id, produto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        produtoService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
